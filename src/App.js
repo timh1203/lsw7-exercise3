dab84a4d0ecf6198ce6972697b3d678e6f56f30a
Timothy Hoang <timh1203@gmail.com>
2018-09-02 20:44:19 -0400

    Add items to state and proptypes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import logo from './logo.svg';
 import './App.css';
+import Cars from "./Cars"
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      cars: [
+        {
+          "id": 1,
+          "make": "Bugatti",
+          "model": "Veyron",
+          "color": "Khaki"
+        },
+        {
+          "id": 2,
+          "make": "Dodge",
+          "model": "Ram 1500",
+          "color": "Indigo"
+        },
+        {
+          "id": 3,
+          "make": "Chevrolet",
+          "model": "Camaro",
+          "color": "Puce"
+        },
+        {
+          "id": 4,
+          "make": "Honda",
+          "model": "Odyssey",
+          "color": "Violet"
+        },
+        {
+          "id": 5,
+          "make": "Infiniti",
+          "model": "I",
+          "color": "Mauv"
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +49,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <List cars={this.state.cars} />
       </div>
     );
   }
